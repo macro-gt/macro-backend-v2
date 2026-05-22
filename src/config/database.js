589a91054210dc8bd4d8a0a11f01a3a8a2d26fd8
macro-gt <macrosoportesistema@gmail.com>
2026-05-22 12:55:46 -0600

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,13 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const connectionString = process.env.DATABASE_URL;
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  connectionString,
+  ssl: process.env.NODE_ENV === 'production' 
+    ? { rejectUnauthorized: false, checkServerIdentity: () => undefined }
+    : false
 });
 
 pool.on('connect', () => {
